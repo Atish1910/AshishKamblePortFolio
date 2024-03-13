44a1523f3b6f87e61936173ac2037fea308b1d91
Ashish Vinayak Kamble <137804106+Atish1910@users.noreply.github.com>
2024-03-14 02:02:41 +0530

    xcv

--- Portfolio/src/app/app.component.ts
+++ Portfolio/src/app/app.component.ts
@@ -10,5 +10,33 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'Portfolio';
+  
+  languages = [
+    { name: 'Javascript', logo: '1' },
+    { name: 'Typescript', logo: '2' },
+    { name: 'Php', logo: '3' },
+    { name: 'Java', logo: '4' },
+    { name: 'C++', logo: '5' },
+    { name: 'C', logo: '6' }
+  ];
+
+  frameworks = [
+    { name: 'Angular V17', logo: '1' },
+    { name: 'Bootstrap 5', logo: '2' },
+    { name: 'Material Design', logo: '3' },
+    { name: 'Css 3', logo: '4' },
+    { name: 'Node Js', logo: '5' },
+    { name: 'Express Js', logo: '6' },
+    { name: 'JQuery', logo: '7' },
+    { name: 'JSON', logo: '8' }
+  ];
+
+  
+  tools = [
+    { name: 'My SQL', logo: '1' },
+    { name: 'MongoDB', logo: '2' },
+    { name: 'Git Hub', logo: '3' },
+    { name: 'Xampp', logo: '4' },
+    { name: 'AWS', logo: '5' },
+  ];
 }
